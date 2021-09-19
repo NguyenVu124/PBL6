e28a9a2853bd2a4442bfa5e6cfbf358c7ec08b7a
NguyenVu124 <hoangnguyenvubk@gmail.com>
2021-09-19 14:35:41 +0700

    first init

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,18 @@
+/* eslint-disable no-undef */
 const mongoose = require("mongoose");
 const app = require("./app");
+require("dotenv").config();
+
+const dbUrl = process.env.DB_LOCAL || process.env.DB_CLOUD;
+const port = process.env.PORT || 3000;
 
 mongoose
-  .connect("mongodb://localhost/practice", {
+  .connect(dbUrl, {
     useNewUrlParser: true,
-
     useUnifiedTopology: true,
   })
   .then(() => console.log("DB connected!"));
 
-app.listen(3000, () => {
-  console.log("App running on port 3000");
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
 });
