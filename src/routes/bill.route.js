f28da41b413c86d35a0d1e64eaeadcc42ec5e516
NguyenVu124 <hoangnguyenvubk@gmail.com>
2021-11-25 16:39:51 +0700

    add bill api docss

--- src/routes/bill.route.js
+++ src/routes/bill.route.js
@@ -4,14 +4,208 @@ const auth = require('../middlewares/auth');
 
 const router = express.Router();
 
-router.route('/:userId').get(auth('getBills'), billController.getBills);
-
 router
   .route('/:userId/:billId')
   .get(auth('getBills'), billController.getBill)
   .patch(auth('updateBill'), billController.updateBill)
   .delete(auth('deleteBill'), billController.deleteBill);
 
-router.route('/:userId').post(auth('createBill'), billController.createBill);
+router.route('/:userId').get(auth('getBills'), billController.getBills).post(auth('createBill'), billController.createBill);
 
 module.exports = router;
+
+/**
+ * @swagger
+ * tags:
+ *   name: Bill
+ *   description: Bill management and retrieval
+ */
+
+/**
+ * @swagger
+ * /bill/{userId}:
+ *   get:
+ *     summary: Get a Bill
+ *     description: Logged in users can fetch only their own user information. Only admins can fetch other users.
+ *     tags: [Bill]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User id
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/User'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ *   post:
+ *     summary: Create a Bill
+ *     description:  Admins and Partner can create Bill
+ *     tags: [Bill]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - email
+ *               - password
+ *               - role
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: must be unique
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 minLength: 8
+ *                 description: At least one number and one letter
+ *               role:
+ *                  type: string
+ *                  enum: [user, admin]
+ *             example:
+ *               name: fake name
+ *               email: fake@example.com
+ *               password: password1
+ *               role: user
+ *     responses:
+ *       "201":
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/User'
+ *       "400":
+ *         $ref: '#/components/responses/DuplicateEmail'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *
+ *
+ */
+
+/**
+ * @swagger
+ * /bill/{userId}/{billId}:
+ *   get:
+ *     summary: Get Bill by userId
+ *     description: Logged in users can fetch only their own user information. Only admins can fetch other users.
+ *     tags: [Bill]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User id
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/User'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ *
+ *   patch:
+ *     summary: Update a Bill
+ *     description: Logged in users can only update their own information. Only admins can update other users.
+ *     tags: [Bill]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: must be unique
+ *               password:
+ *                 type: string
+ *                 format: password
+ *                 minLength: 8
+ *                 description: At least one number and one letter
+ *             example:
+ *               name: fake name
+ *               email: fake@example.com
+ *               password: password1
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/User'
+ *       "400":
+ *         $ref: '#/components/responses/DuplicateEmail'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ *
+ *   delete:
+ *     summary: Delete a Bill
+ *     description: Logged in users can delete only themselves. Only admins can delete other users.
+ *     tags: [Bill]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: User id
+ *     responses:
+ *       "200":
+ *         description: No content
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+
+ */
